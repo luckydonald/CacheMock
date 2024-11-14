b73a7e8f304649b7146922be296758f0ee6c9365
luckydonald <m1-mac-2024._.code@luckydonald.de>
2024-11-15 00:43:29 +0100

    Initial storage.

---
+++ storage.py
@@ -0,0 +1,12 @@
+def is_setup() -> bool:
+    return False
+# end def
+
+def get_setup_proxy() -> str | None:
+    return None
+# end def
+
+
+def set_setup_proxy(proxy: str) -> None:
+    return None
+# end def
