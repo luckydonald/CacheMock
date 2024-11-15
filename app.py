aaaca2958f6d614bc995405aa5f2bc14e48c25bd
luckydonald <m1-mac-2024._.code@luckydonald.de>
2024-11-15 01:03:11 +0100

    Blueprint for the settings.

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
 from html import escape
 
 from flask import Flask, url_for, request, redirect
+from flask.blueprints import Blueprint
 
 import storage
 app = Flask(__name__)
 
 HIDDEN_PATH = '__proxy__'
 
+ui = Blueprint('ui', __name__)
+app.register_blueprint(ui, url_prefix=HIDDEN_PATH)
+
+
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     if not storage.is_setup():
-        url = url_for(settings_index.__name__)
+        url = url_for(f"{ui.name},{settings_index.__name__}")
         return f"""YOU NEED TO SETUP FIRST\n\n<br><a href="{url}">Click here</a> or<br>\n\ngo to: {url}"""
     # end if
     return 'You want path: %s' % path
 # end def
 
-
-
-@app.route(f'/{HIDDEN_PATH}/')
+@ui.route(f'/')
 def settings_index():
     if not storage.is_setup():
-        url = url_for(proxy_setup.__name__)
+        url = url_for(f"{ui.name},{proxy_setup.__name__}")
         # redirect to proxy_setup
         return redirect(url, code=302)  # temp redirect
     return f"""
     <ul>
-    <li><a href="{escape(url_for(proxy_setup.__name__))}">Proxy URL settings</a></li>
+    <li><a href="{escape(url_for(f"{ui.name},{proxy_setup.__name__}"))}">Proxy URL settings</a></li>
     </ul>
     """
 # end def
 
 
-@app.route(f'/{HIDDEN_PATH}/setup/proxy', methods=['GET', 'POST'])
+@ui.route(f'/setup/proxy', methods=['GET', 'POST'])
 def proxy_setup():
     if request.method != "POST":
         # return a form with a single input field for setting the proxy
@@ -61,7 +65,7 @@ def proxy_setup():
         return f"""
             <h1>Proxy URL settings</h1>
             <h2>✅ Proxy settings saved</h2>
-            <a href="{url_for(settings_index.__name__)}">back</a>
+            <a href="{url_for(f"{ui.name},{settings_index.__name__}")}">back</a>
         """
     # end if
 # end def
